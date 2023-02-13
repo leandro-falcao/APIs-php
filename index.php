2f21823638cd08b6ec157e004509c24b0c6c1eb6
leandro-falcao <leandrofalcaoti@gmail.com>
2023-02-13 18:23:34 -0300

    essa parte de um a mero consumo de API pura

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once("api-get-file-contents.php");
+//        $nome = $_GET['nome'];
+//        // $nome = "le";
+   
+//    $sites = ["https://randomuser.me/api", "https://api.agify.io/?name={$nome}"];
+//        $response = file_get_contents($sites[1]);
+//        var_dump($nome);
+//        $dados = json_decode($response, true);
+      
+   
+//        // echo $dados["info"]["seed"], "\n";
+//        // echo $response, "\n";
+//        echo $response, "\n";
