b744f3833a85323c081da3c656339942488f10e8
leandro-falcao <leandrofalcaoti@gmail.com>
2023-02-14 17:06:14 -0300

    imagem e comment sobre o json

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-// chamando o arquivo da api na pasta de api file get contens
+    /**
+    chamando o arquivo da api na pasta de api file get contens, ela esta sendo validada por postman
+    1) usar método GET no postman, com endereço http://localhost 
+    2) no body em raw, passa um json por exemplo: { "nome":"leandro" }
+    **/
     require_once("api-file-get-contents/file-get-contents.php");
 // chamando a api de requisição com função php file get contents
     print(call());
